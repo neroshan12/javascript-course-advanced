04bed1a656d0d8339260a1587a5cfed89f062fe2
Nero <neroshan12@gmail.com>
2017-12-18 12:08:52 +0000

    Add primitives vs. objects

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ console.log(Messi.lastName);
 */
 
 // Object.create
-
+/*
 var personProto = {
   calculateAge: function() {
     conosle.log(2017 - this.yearOfBirth);
@@ -55,3 +55,46 @@ var neymar = Object.create(personProto, {
   yearOfBirth: { value: 1992 },
   job: { value: 'footballer' }
 });
+*/
+
+// Primitives vs Objects
+
+// Primitives = numbers, strings, booleans, undefinded and null
+// everything else = Object
+
+// variables containing primitives hold that data inside the variable itself
+// objects: variables assosciated with objects do not actually contain the object but a reference to a place in memory where that object is stored
+
+// primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a); // 46
+console.log(b); // 23
+
+//objects
+obj1 = {
+  name: 'Nero',
+  age: 26
+};
+
+var obj2 = obj1; // no new object was created here, only a reference to it
+obj1.age = 30;
+console.log(obj1.age); // 30
+console.log(obj2.age); // 30
+
+// functions
+var age = 27;
+var obj = {
+  name: 'messi',
+  city: 'Barcelona'
+};
+
+function change(a, b) {
+  (a = 30), (b.city = 'Manchester');
+}
+
+change(age, obj);
+
+console.log(age); // 27
+console.log(obj.city); // Manchester
