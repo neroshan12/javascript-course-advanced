4fb20aac4c59282e84566e47a173acb010c00663
Nero <neroshan12@gmail.com>
2017-12-27 15:20:15 +0000

    Add bind, call and apply

--- script.js
+++ script.js
@@ -224,3 +224,90 @@ var neymar = Object.create(personProto, {
 // }
 //
 // interviewQuestion('teacher')('Nero');
+
+// Lecture: Bind, call and apply
+
+// var john = {
+//   name: 'John',
+//   age: 26,
+//   job: 'teacher',
+//   presentation: function(style, timeOfDay) {
+//     if (style === 'formal') {
+//       console.log(
+//         'Good ' +
+//           timeOfDay +
+//           ", Ladies and gentlemen! I'm " +
+//           this.name +
+//           ", I'm a " +
+//           this.job +
+//           " and I'm " +
+//           this.age +
+//           ' years old.'
+//       );
+//     } else if (style === 'friendly') {
+//       console.log(
+//         "Hey! What's up? I'm " +
+//           this.name +
+//           ", I'm a " +
+//           this.job +
+//           " and I'm " +
+//           this.age +
+//           ' years old. Have a nice ' +
+//           timeOfDay +
+//           '.'
+//       );
+//     }
+//   }
+// };
+//
+// var emily = {
+//   name: 'Emily',
+//   age: 35,
+//   job: 'designer'
+//   // want to use presentation method for emily object, we can use the call method. First argument is setting the this variable
+// };
+//
+// john.presentation('formal', 'morning');
+//
+// john.presentation.call(emily, 'friendly', 'afternoon'); // using emily here sets all the 'this' variable in john (this.name, this. age etc to emily)
+// // called method borrowing
+//
+// // also an apply method which takes an array e.g john.presentation.apply(emily, ['friendly', 'afternoon']) # if the method expects to receive an array
+//
+// // bind also allows us to set this method explicitly
+// // bind does not immediately call the fucntiuon but generates a copy of it so it can be stored somewhere
+// // useful when create functions with pre-set arguments
+// // below = 'carrying' : when function is created based on another function but with pre-set parameters
+//
+// var johnFriendly = john.presentation.bind(john, 'friendly');
+//
+// johnFriendly('morning');
+// johnFriendly('night');
+//
+// var emilyFormal = john.presentation.bind(emily, 'formal');
+//
+// emilyFormal('afternoon');
+//
+// var years = [1990, 1965, 1937, 2005, 1998];
+//
+// function arrayCalc(arr, fn) {
+//   var arrRes = [];
+//   for (var i = 0; i < arr.length; i++) {
+//     arrRes.push(fn(arr[i]));
+//   }
+//   return arrRes;
+// }
+//
+// function calculateAge(el) {
+//   return 2017 - el;
+// }
+//
+// function isFullAge(limit, el) {
+//   return el >= limit;
+// }
+//
+// var ages = arrayCalc(years, calculateAge);
+//
+// var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20)); // copy of isFullAge function passed in with a copy of the function
+// console.log(ages);
+// console.log(fullJapan);
